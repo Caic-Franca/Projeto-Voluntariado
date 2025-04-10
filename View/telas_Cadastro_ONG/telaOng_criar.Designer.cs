575ed5e17dae2b845ec35bc146a0be2de29aa433
P4ivvaz <livia.psousa@senacsp.edu.br>
2025-04-09 22:11:04 -0300

    edição tela 1

--- View/telas_Cadastro_ONG/telaOng_criar.Designer.cs
+++ View/telas_Cadastro_ONG/telaOng_criar.Designer.cs
@@ -30,85 +30,122 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(telaOng_criar));
             this.btn_OngMenuVolta = new System.Windows.Forms.Button();
-            this.btn_VltCriarONG = new System.Windows.Forms.Button();
-            this.btn_LogOng = new System.Windows.Forms.Button();
-            this.qst_VLT = new System.Windows.Forms.Label();
-            this.bnf_VLT = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_OngMenuVolta
             // 
-            this.btn_OngMenuVolta.Location = new System.Drawing.Point(685, 410);
+            this.btn_OngMenuVolta.Location = new System.Drawing.Point(592, 299);
             this.btn_OngMenuVolta.Name = "btn_OngMenuVolta";
-            this.btn_OngMenuVolta.Size = new System.Drawing.Size(75, 23);
+            this.btn_OngMenuVolta.Size = new System.Drawing.Size(84, 41);
             this.btn_OngMenuVolta.TabIndex = 11;
             this.btn_OngMenuVolta.Text = "Voltar";
             this.btn_OngMenuVolta.UseVisualStyleBackColor = true;
             this.btn_OngMenuVolta.Click += new System.EventHandler(this.btn_VltMenuVolta_Click);
             // 
-            // btn_VltCriarONG
+            // panel1
             // 
-            this.btn_VltCriarONG.Location = new System.Drawing.Point(376, 324);
-            this.btn_VltCriarONG.Name = "btn_VltCriarONG";
-            this.btn_VltCriarONG.Size = new System.Drawing.Size(98, 41);
-            this.btn_VltCriarONG.TabIndex = 10;
-            this.btn_VltCriarONG.Text = "Não, criar conta!";
-            this.btn_VltCriarONG.UseVisualStyleBackColor = true;
-            this.btn_VltCriarONG.Click += new System.EventHandler(this.btn_VltCriar_Click);
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(149, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(559, 347);
+            this.panel1.TabIndex = 12;
             // 
-            // btn_LogOng
+            // button1
             // 
-            this.btn_LogOng.Location = new System.Drawing.Point(126, 324);
-            this.btn_LogOng.Name = "btn_LogOng";
-            this.btn_LogOng.Size = new System.Drawing.Size(98, 41);
-            this.btn_LogOng.TabIndex = 9;
-            this.btn_LogOng.Text = "Sim, acessar comunidade!";
-            this.btn_LogOng.UseVisualStyleBackColor = true;
-            this.btn_LogOng.Click += new System.EventHandler(this.btn_VltLog_Click);
+            this.button1.Location = new System.Drawing.Point(391, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 41);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Voltar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // qst_VLT
+            // button2
             // 
-            this.qst_VLT.AutoSize = true;
-            this.qst_VLT.Location = new System.Drawing.Point(213, 274);
-            this.qst_VLT.Name = "qst_VLT";
-            this.qst_VLT.Size = new System.Drawing.Size(200, 13);
-            this.qst_VLT.TabIndex = 8;
-            this.qst_VLT.Text = "Você já faz parte da nossa comunidade?";
-            this.qst_VLT.Click += new System.EventHandler(this.qst_VLT_Click);
+            this.button2.Location = new System.Drawing.Point(180, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 41);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Não, criar conta!";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // bnf_VLT
+            // button3
             // 
-            this.bnf_VLT.AutoSize = true;
-            this.bnf_VLT.Location = new System.Drawing.Point(123, 98);
-            this.bnf_VLT.Name = "bnf_VLT";
-            this.bnf_VLT.Size = new System.Drawing.Size(545, 117);
-            this.bnf_VLT.TabIndex = 7;
-            this.bnf_VLT.Text = resources.GetString("bnf_VLT.Text");
-            this.bnf_VLT.Click += new System.EventHandler(this.bnf_VLT_Click);
+            this.button3.Location = new System.Drawing.Point(10, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 41);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Sim, acessar comunidade!";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Você já faz parte da nossa comunidade?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(545, 130);
+            this.label2.TabIndex = 12;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(217, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
             // 
             // telaOng_criar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
             this.ClientSize = new System.Drawing.Size(864, 551);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_OngMenuVolta);
-            this.Controls.Add(this.btn_VltCriarONG);
-            this.Controls.Add(this.btn_LogOng);
-            this.Controls.Add(this.qst_VLT);
-            this.Controls.Add(this.bnf_VLT);
             this.Name = "telaOng_criar";
             this.Text = "telaOng_criar";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_OngMenuVolta;
-        private System.Windows.Forms.Button btn_VltCriarONG;
-        private System.Windows.Forms.Button btn_LogOng;
-        private System.Windows.Forms.Label qst_VLT;
-        private System.Windows.Forms.Label bnf_VLT;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
