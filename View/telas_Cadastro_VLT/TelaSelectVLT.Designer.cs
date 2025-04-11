96f019cdbebb420664017c554483c197f4232268
P4ivvaz <livia.psousa@senacsp.edu.br>
2025-04-10 21:40:41 -0300

    ok

--- View/telas_Cadastro_VLT/TelaSelectVLT.Designer.cs
+++ View/telas_Cadastro_VLT/TelaSelectVLT.Designer.cs
@@ -41,7 +41,7 @@
             // 
             // btn_VltMenuVolta
             // 
-            this.btn_VltMenuVolta.Location = new System.Drawing.Point(836, 494);
+            this.btn_VltMenuVolta.Location = new System.Drawing.Point(837, 484);
             this.btn_VltMenuVolta.Name = "btn_VltMenuVolta";
             this.btn_VltMenuVolta.Size = new System.Drawing.Size(75, 23);
             this.btn_VltMenuVolta.TabIndex = 5;
@@ -57,7 +57,7 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(178, 12);
+            this.panel1.Location = new System.Drawing.Point(209, 35);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(566, 436);
             this.panel1.TabIndex = 6;
