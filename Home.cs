41783a68ad97f65b142457080fe88bd820c4c00f
Rafael D' Carvalho <carvalhorafael2702@gmail.com>
2025-04-23 20:40:29 -0300

    Add files via upload

--- Home.cs
+++ Home.cs
@@ -42,5 +42,25 @@ namespace Projeto_Voluntariado
             TelaSelectVLT telaSelectvlt = new TelaSelectVLT();
             telaSelectvlt.Show();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
