a383826befd981dc0edcfc9310ba0d4346b113aa
Rafael D' Carvalho <carvalhorafael2702@gmail.com>
2025-04-27 04:20:54 -0300

    .

--- View/telas_Cadastro_ONG/confirmcadOng.Designer.cs
+++ View/telas_Cadastro_ONG/confirmcadOng.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfirmcadOng));
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,19 +37,19 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(204)))), ((int)(((byte)(233)))));
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(109, 63);
+            this.panel1.Location = new System.Drawing.Point(89, 45);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(249, 131);
+            this.panel1.Size = new System.Drawing.Size(306, 161);
             this.panel1.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(160, 90);
+            this.button1.Location = new System.Drawing.Point(108, 83);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(85, 41);
             this.button1.TabIndex = 5;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,9 +58,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 17);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.Size = new System.Drawing.Size(258, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Parabéns, seu cadastro foi criado!";
             // 
@@ -67,9 +69,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SaddleBrown;
-            this.ClientSize = new System.Drawing.Size(455, 293);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(179)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(508, 274);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ConfirmcadOng";
             this.Text = "confirmcadOng";
             this.panel1.ResumeLayout(false);
