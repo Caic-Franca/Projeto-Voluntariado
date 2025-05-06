dce3361b3ec02f862a7da74718416bfd31158dfc
P4ivvaz <livia.psousa@senacsp.edu.br>
2025-05-05 22:14:58 -0300

    Iniciado o processo de adição do banco de dados no projeto

---
+++ Controller/AuthController.cs
@@ -0,0 +1,41 @@
+﻿//using Projeto_Voluntariado.Models;
+//using Projeto_Voluntariado.Services;
+//using System;
+
+//namespace MyApp.Controllers
+//{
+//    public class AuthController
+//    {
+//        private readonly AuthService _authService;
+
+//        public AuthController(AuthService authService)
+//        {
+//            _authService = authService;
+//        }
+
+//        public Usuario Login(string email, string password)
+//        {
+//            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+//            {
+//                throw new ArgumentException("Email e senha são obrigatórios");
+//            }
+
+//            return _authService.Authenticate(email, password);
+//        }
+
+//        public bool Register(Usuario usuario, string password)
+//        {
+//            if (usuario == null)
+//            {
+//                throw new ArgumentNullException(nameof(usuario));
+//            }
+
+//            if (string.IsNullOrEmpty(password))
+//            {
+//                throw new ArgumentException("Senha é obrigatória");
+//            }
+
+//            return _authService.Register(usuario, password);
+//        }
+//    }
+//}
